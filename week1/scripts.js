c0526312e2ff2c70f3f4d0b7878a35e901bcd7de
DmitriySokolov2023 <sokolovoff2023@gmail.com>
2026-01-22 17:04:53 +0300

    Update scripts.js

--- week1/scripts.js
+++ week1/scripts.js
@@ -1,10 +1,24 @@
-const btn = document.getElementById('myButton')
-const sumDiv = document.getElementById('sum')
-let count = 0
+// const btn = document.getElementById('myButton')
+// const sumDiv = document.getElementById('sum')
+// let count = 0
 
-btn.addEventListener('click', () => {
-	if (sumDiv) {
-		count += 1
-		sumDiv.textContent = `Button clicked ${count} times`
-	}
-})
+// btn.addEventListener('click', () => {
+// 	if (sumDiv) {
+// 		count += 1
+// 		sumDiv.textContent = `Button clicked ${count} times`
+// 	}
+// })
+
+const obj = {
+	a: 25,
+}
+
+const objCopy = obj
+
+objCopy.a = 50
+
+console.log()
+
+const arr = [1, 2, 3]
+
+arr.map(num => console.log(num))
